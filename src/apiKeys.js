da00cb2cc14de6aeb00ab1f7bd54bfe39f9d0656
Govind Singh <govindsingh@kgpian.iitkgp.ac.in>
2023-07-04 19:43:55 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "a2a00beb21be5d7ff300c7b289d92df6",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
